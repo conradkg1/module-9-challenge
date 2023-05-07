97967aeafa1847a05bfaaca933072983afcd0876
conrad <conradkg1@gmail.com>
2023-05-06 20:03:48 -0500

    edit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,116 @@
-// TODO: Include packages needed for this application
+// // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
+// // Function call to initialize app
+// init();
+
+// const { mkdir, writeFile } = require("node:fs/promises");
+// const inquirer = require("inquirer");
+// const generateMarkdown = require("./utils/generateMarkdown.js");
+
+const questions = [
+    {
+        name: 'title',
+        type: 'input',
+        message: 'What is the title of your project?',
+        validate(answer) {
+            if (!answer) {
+                return 'Your project must have a title'
+            }
+            return true;
+        }
+    },
+    {
+        name: 'githubUsername',
+        type: 'input',
+        message: 'What is your github username?',
+    },
+    {
+        name: 'emailInQuestions',
+        type: 'list',
+        message: 'Would you like to put your email address in the `Questions` section of the README?',
+        choices: ["yes", "no"]
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Enter your email address: ',
+        when(answers) {
+            return answers.emailInQuestions === "yes";
+        },
+    },
+    {
+        name: 'license',
+        type: 'list',
+        message: 'How is your project licensed?',
+        choices: ['MIT', 'ISC', 'BSD2', 'BSD3', 'GPLv2', 'GPLv3', 'Apache', 'None' ],
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Briefly describe your project: ',
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'What are the installation instructions for your project?',
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'What are the usage instructions for your project?',
+    },
+    {
+        name: 'contributing',
+        type: 'input',
+        message: 'How can others contribute to your project?',
+    },
+    {
+        name: 'tests',
+        type: 'input',
+        message: 'How can users test your project?',
+    },
+];
+
+async function createBuildDir() {
+    try {
+        // mkdir rejects if ./build/ exists when (default) { recursive: false }
+        const createDir = await mkdir('./build/', { recursive: true });
+        console.log(`created ${createDir}`);
+        return createDir;
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+async function writeToFile(fileName, data) {
+    try {
+        return await writeFile(fileName, data);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            createBuildDir()
+                .then(() => {
+                    writeToFile(`./build/README.md`, generateMarkdown(answers));
+                });
+        }) 
+        .catch((err) => {
+            if (err) throw err;
+        });
+}
+
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,130 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
+
+// `;
+// }
+
+// module.exports = generateMarkdown;
+
+const licenses = {
+  'MIT': {
+    name: 'MIT-0',
+    text: 'MIT No Attribution License',
+    link: 'https://opensource.org/licenses/MIT',
+    badge: 'https://img.shields.io/badge/License-MIT-yellow.svg',
+  },
+  'ISC': {
+    name: 'ISC',
+    text: 'ISC License (ISC)',
+    link: 'https://opensource.org/licenses/ISC',
+    badge: 'https://img.shields.io/badge/License-ISC-blue.svg',
+  },
+  'BSD2': {
+    name: 'BSD2',
+    text: 'The 2-Clause BSD License',
+    link: 'https://opensource.org/license/bsd-license-php/',
+    badge: 'https://img.shields.io/badge/License-BSD_2--Clause-orange.svg',
+  },
+  'BSD3': {
+    name: 'BSD3',
+    text: 'The 3-Clause BSD License',
+    link: 'https://opensource.org/licenses/BSD-3-Clause',
+    badge: 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg',
+  },
+  'GPLv2': {
+    name: 'GPLv2',
+    text: 'GNU General Public License, version 2',
+    link: 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html',
+    badge: 'https://img.shields.io/badge/License-GPL_v2-blue.svg',
+  },
+  'GPLv3': {
+    name: 'GPLv3',
+    text: 'GNU Public License, version 3',
+    link: 'https://www.gnu.org/licenses/gpl-3.0',
+    badge: 'https://img.shields.io/badge/License-GPLv3-blue.svg',
+  },
+  'Apache': {
+    name: 'Apache v2.0',
+    text: 'Apache License, version 2.0',
+    link: 'https://opensource.org/license/apache2-0-php/',
+    badge: 'https://img.shields.io/badge/License-Apache_2.0-blue.svg',
+  },
+};
+
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    const li = licenses[license];
+    return `[![License: ${li.name}](${li.badge})](${li.link})`;
+  }
+  return '';
+}
+
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    const li = licenses[license];
+    return `[${li.link}](${li.link})`;
+  }
+  return '';
+}
+
+function renderLicenseSection(license) {
+  const licenseLink = renderLicenseLink(license);
+  if (licenseLink) {
+    const li = licenses[license];
+    return `${li.text}: ${licenseLink}`;
+  }
+  return 'This project has not chosen a license yet.';
+}
+
+function renderQuestions(email, userName) {
+  const mail = `, email: [${email}](${email})` ?? '';
+  return `Contact [${userName}](https://github.com/${userName})` + mail; 
+}
+
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+# ${data.title}
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+### [Installation](#installation)
+<a name="installation"></a>
+${data.installation}
+### [Usage](#usage)
+<a name="usage"></a>
+${data.usage}
+### [License](#license)
+<a name="license"></a>
+${renderLicenseSection(data.license)}
+### [Contributing](#contributing)
+<a name="contributing"></a>
+${data.contributing}
+### [Tests](#tests)
+<a name="tests"></a>
+${data.tests}
+### [Questions](#questions)
+<a name="questions"></a>
+${renderQuestions(data.email, data.githubUsername)}
 `;
 }
 
